dbde8e27db7dbcacf27502efd24aad12a17de033
xiarongx <xiarongx@uci.edu>
2018-04-12 11:45:01 -0400

    add 4 packages from nuget server

--- MultiSourceProject/Program.cs
+++ MultiSourceProject/Program.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PrivateNugetMath;
 using Evaluator;
+
 namespace MultiSourceProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MathClass mathClass = new MathClass();
-            Console.WriteLine("Next line uses private nuget package from dev server.");
-            Console.WriteLine("5 times 5 equals: " + mathClass.multiplication(5, 5));
 
             Eval eval = new Eval();
             string mathExpression = "2 * 3 + 4";
@@ -22,6 +19,15 @@ namespace MultiSourceProject
             Console.WriteLine("Next line uses a public nuget package from nuget.org.");
             Console.WriteLine("2 * 3 + 4 = " + result);
 
+            ACReditPlus.TestLib1.returnString returnStringAP1 = new ACReditPlus.TestLib1.returnString();
+            ACReditPlus.TestLib2.returnString returnStringAP2 = new ACReditPlus.TestLib2.returnString();
+            Triad.TestLib1.returnString returnStringT1 = new Triad.TestLib1.returnString();
+            Triad.TestLib2.returnString returnStringT2 = new Triad.TestLib2.returnString();
+
+            Console.WriteLine(returnStringAP1.TimeAndLocation());
+            Console.WriteLine(returnStringAP2.TimeAndLocation());
+            Console.WriteLine(returnStringT1.TimeAndLocation());
+            Console.WriteLine(returnStringT2.TimeAndLocation());
 
             Console.ReadLine();
         }
