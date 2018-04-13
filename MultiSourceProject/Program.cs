6b459586f05e5e0680e76bb5bb618ab7bbc385ed
xiarongx <xiarongx@uci.edu>
2018-04-13 15:35:10 -0400

    uses new package ACReditPlus.TestLib 3.0.0

--- MultiSourceProject/Program.cs
+++ MultiSourceProject/Program.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using ACReditPlus.TestLib1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Evaluator;
 
 namespace MultiSourceProject
 {
@@ -12,22 +12,12 @@ namespace MultiSourceProject
         static void Main(string[] args)
         {
 
-            Eval eval = new Eval();
-            string mathExpression = "2 * 3 + 4";
-            var result = eval.Evaluate(mathExpression);
+            Console.WriteLine("This console app uses private NuGet package ACReditPlus.TestLib1");
+            Console.WriteLine("Which has two dependencies: ACReditPlus.TestLib2 (private) and Evaluator (public)");
 
-            Console.WriteLine("Next line uses a public nuget package from nuget.org.");
-            Console.WriteLine("2 * 3 + 4 = " + result);
+            ReturnString timeAndLocation = new ReturnString();
 
-            ACReditPlus.TestLib1.returnString returnStringAP1 = new ACReditPlus.TestLib1.returnString();
-            ACReditPlus.TestLib2.returnString returnStringAP2 = new ACReditPlus.TestLib2.returnString();
-            Triad.TestLib1.returnString returnStringT1 = new Triad.TestLib1.returnString();
-            Triad.TestLib2.returnString returnStringT2 = new Triad.TestLib2.returnString();
-
-            Console.WriteLine(returnStringAP1.TimeAndLocation());
-            Console.WriteLine(returnStringAP2.TimeAndLocation());
-            Console.WriteLine(returnStringT1.TimeAndLocation());
-            Console.WriteLine(returnStringT2.TimeAndLocation());
+            Console.WriteLine(timeAndLocation.TimeAndLocation());
 
             Console.ReadLine();
         }
